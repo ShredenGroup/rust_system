9c7f32444c59287c1e0e592ec8314236c21e3c24
hanboli <litterpigger@gmail.com>
2025-11-10 23:01:19 +0800

    feat:ml

--- src/strategy/order_book_taker/ml_v1.rs
+++ src/strategy/order_book_taker/ml_v1.rs
@@ -2,3 +2,9 @@ use crate::models::{StrategyContext, StrategyPosition};
 use hmac::digest::typenum::Max;
 use ta::indicators::{Maximum, Minimum, SimpleMovingAverage, StandardDeviation,ZScore};
 use ta::{Close, High, Open};
+use trusty::GradientBoostedDecisionTrees;
+pub struct MLV1Strategy {
+    pub cxt: StrategyContext,
+    pub position: StrategyPosition,
+    pub gbt: GradientBoostedDecisionTrees,
+}
